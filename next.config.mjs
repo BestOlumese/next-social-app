0440ff737dc1817febad6465d2d1f123555a4709
BestOlumese <bestolumese@gmail.com>
2024-09-25 13:50:54 +0100

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        staleTimes:  {
+            dynamic: 30,
+        },
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+          config.externals.push('@node-rs/argon2'); // Exclude it from the server bundle
+        }
+        return config;
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "utfs.io",
+                pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
+            },
+        ]
+    }
+};
+
+export default nextConfig;
